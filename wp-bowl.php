f111f35327972d050c50749897372a078bfb5660
Alexis Bouhet <alexis@les-hallucines.studio>
2022-03-22 19:32:41 +0100

    Patched body style for admin

--- wp-bowl.php
+++ wp-bowl.php
@@ -11,7 +11,7 @@
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       bowl
  * Domain Path:       /cms
- * Version:           1.0.1
+ * Version:           1.0.2
  * Copyright:         © 2022 Alexis Bouhet
  */
 
