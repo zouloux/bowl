19cee74a81b35d80bd648f00d54ec5f4631b4bf3
Alexis Bouhet <alexis@les-hallucines.studio>
2022-11-02 19:46:50 +0100

    Screen options enabled back because admin users needs to enable excerpt meta box, etc

--- wp-bowl.php
+++ wp-bowl.php
@@ -11,7 +11,7 @@
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       bowl
  * Domain Path:       /cms
- * Version:           1.0.33
+ * Version:           1.0.34
  * Copyright:         © 2022 Alexis Bouhet
  */
 
