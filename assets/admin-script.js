e10278b1468d64c04f03c9c5294ed49bc72bb273
Alexis Bouhet <alexis@les-hallucines.studio>
2023-01-06 13:50:22 +0100

    Patched meta boxes and patched instructions in clean mode

--- assets/admin-script.js
+++ assets/admin-script.js
@@ -1,39 +1,20 @@
 
-function initCustomMetaBoxBehavior ( $ )
-{
-  // Because we removed default draggable behavior for meta boxes
-  // We need to add back open / close feature.
-  // No need to filter acf-metabox here since we also need it for them
-  $('.postbox .handle-actions button.handlediv').on('click', function (e) {
-    $(e.currentTarget).parent().parent().parent().toggleClass('closed');
-  });
-
-  // By default, we close yoast panel
-  $('.postbox.yoast').addClass('closed');
-}
-
-function patchFlexibleBehavior ( $ )
-{
+function patchFlexibleBehavior ( $ ) {
   // Invert flexible layouts collapsed state when we click on flexible content title
   var $flexibleLabel = $('.acf-field-flexible-content > .acf-label > label');
-  if ( $flexibleLabel.length > 0 )
-  {
-    var opened = true;
-    var collapsedClass = '-collapsed';
-    $flexibleLabel
-    .text( $flexibleLabel.text() + " ↕️" )
-    .on('click', function (e) {
-      var $layouts = $(e.currentTarget).parent().parent().find('.values > .layout');
-      opened = !opened;
-      opened ? $layouts.removeClass(collapsedClass) : $layouts.addClass(collapsedClass);
-    })
-  }
+  if ( $flexibleLabel.length === 0 ) return
+  var opened = true;
+  var collapsedClass = '-collapsed';
+  $flexibleLabel
+  .text( $flexibleLabel.text() + " ↕️" )
+  .on('click', function (e) {
+    var $layouts = $(e.currentTarget).parent().parent().find('.values > .layout');
+    opened = !opened;
+    opened ? $layouts.removeClass(collapsedClass) : $layouts.addClass(collapsedClass);
+  })
 }
 
 jQuery(document).ready( function ($) {
-  // Check if we removed postbox script at admin_init (in admin.config.php)
-  if (window._customMetaboxBehavior) {
-    initCustomMetaBoxBehavior($);
-    patchFlexibleBehavior($)
-  }
+  if (!window._customMetaboxBehavior) return;
+  patchFlexibleBehavior($)
 });
