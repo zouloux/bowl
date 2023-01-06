1333e5332c8ec4eb9b68c440225342cb1e79b94d
Alexis Bouhet <alexis@les-hallucines.studio>
2023-01-06 14:10:17 +0100

    Patched styles for list

--- wp-bowl.php
+++ wp-bowl.php
@@ -11,7 +11,7 @@
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       bowl
  * Domain Path:       /cms
- * Version:           1.1.13
+ * Version:           1.1.14
  * Copyright:         © 2022 Alexis Bouhet
  */
 
