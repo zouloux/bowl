46258aa4eff7b22966e491bb29ff7c065b9dbc5b
Alexis Bouhet <alexis@les-hallucines.studio>
2023-01-12 14:48:39 +0100

    Patched admin style and scripts

--- assets/admin-script.js
+++ assets/admin-script.js
@@ -2,15 +2,16 @@
 function patchFlexibleBehavior ( $ ) {
   // Invert flexible layouts collapsed state when we click on flexible content title
   var $flexibleLabel = $('.acf-field-flexible-content > .acf-label > label');
-  if ( $flexibleLabel.length === 0 ) return
-  var opened = true;
-  var collapsedClass = '-collapsed';
-  $flexibleLabel
-  .text( $flexibleLabel.text() + " ↕️" )
-  .on('click', function (e) {
-    var $layouts = $(e.currentTarget).parent().parent().find('.values > .layout');
-    opened = !opened;
-    opened ? $layouts.removeClass(collapsedClass) : $layouts.addClass(collapsedClass);
+  $flexibleLabel.each(function (i, el) {
+    var opened = true;
+    var collapsedClass = '-collapsed';
+    $(el)
+      .text( $(el).text() + " ↕️" )
+      .on('click', function (e) {
+        var $layouts = $(e.currentTarget).parent().parent().find('.values > .layout');
+        opened = !opened;
+        opened ? $layouts.removeClass(collapsedClass) : $layouts.addClass(collapsedClass);
+      })
   })
 }
 
