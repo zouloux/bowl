915f198357244691aaac4282d416f893bcb970f9
Alexis Bouhet <alexis@les-hallucines.studio>
2023-02-15 15:14:04 +0100

    Patched bowl filter for conditional group

--- wp-bowl.php
+++ wp-bowl.php
@@ -11,7 +11,7 @@
  * License URI:       https://opensource.org/licenses/MIT
  * Text Domain:       bowl
  * Domain Path:       /cms
- * Version:           1.2.6
+ * Version:           1.2.7
  * Copyright:         © 2022 Alexis Bouhet
  */
 
