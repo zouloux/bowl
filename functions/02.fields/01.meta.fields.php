affae860d44887b498632da95b835d786fb584a3
Alexis Bouhet <alexis@les-hallucines.studio>
2023-07-13 14:56:53 +0200

    Added an option for full title override into meta snippet

--- functions/02.fields/01.meta.fields.php
+++ functions/02.fields/01.meta.fields.php
@@ -1,18 +1,19 @@
 <?php
 
-use Extended\ACF\Fields\Image;
 use Extended\ACF\Fields\Text;
 use Extended\ACF\Fields\Textarea;
 
 // ----------------------------------------------------------------------------- META FIELDS
 
-function bowl_create_meta_fields_group ( string $title = 'SEO and share' ) {
+function bowl_create_meta_fields_group ( string $title = 'SEO and share', $showTitleOverride = true ) {
 	$group = new BowlGroupFields( $title );
 	$group->multiLang();
-	$group->fields([
+	$fields = [
 		Textarea::make(bowl_translate_label("Meta description"), bowl_translate_field('description'))
 			->rows(3)
 			->instructions("For SEO only. Optional."),
+		$showTitleOverride ? Text::make(bowl_translate_label("Title override"), bowl_translate_field('title'))
+			->instructions("Override head title tag. Useful to separate post title from title tag. Title template will be ignored.") : null,
 		Text::make(bowl_translate_label("Share title"), bowl_translate_field('shareTitle'))
 			->instructions("For Facebook and Twitter share.<br>Will use page title by default."),
 		Textarea::make(bowl_translate_label("Share description"), bowl_translate_field('shareDescription'))
@@ -20,7 +21,9 @@ function bowl_create_meta_fields_group ( string $title = 'SEO and share' ) {
 			->instructions("For Facebook and Twitter share.<br>Will use meta description by default."),
 		bowl_create_image_field("Share image", 'shareImage')
 			->instructions("For Facebook and Twitter"),
-	]);
+	];
+	$fields = array_filter( $fields, fn ($item) => $item !== null );
+	$group->fields( $fields );
 	return $group;
 }
 
